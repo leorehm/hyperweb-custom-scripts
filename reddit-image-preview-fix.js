b23f67a452fa919fb2cc2e613a050f80f88c7103
leorehm <48659882+leorehm@users.noreply.github.com>
2023-07-25 00:33:43 +0200

    Update reddit-image-preview-fix.js

--- reddit-image-preview-fix.js
+++ reddit-image-preview-fix.js
@@ -3,12 +3,6 @@
 // @match https://i.redd.it/*
 // ==/UserScript==
 
-
-alert("its loaded!");
-
-window.addEventListener('load', function () {
-
   
-  if (document.getElmentsByTagName("img")[0] = window.location.href) 
+if (document.getElmentsByTagName("img")[0] = window.location.href) 
   document.body.innerHTML = document.getElementsByTagName("img")[0].outerHTML;
-})
